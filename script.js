cc54a9e0847e18b7c4e93c718962cc079b51b9e0
NCSanto01 <78079809+NCSanto01@users.noreply.github.com>
2022-02-23 20:02:28 +0100

    Update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let datos=[];
+let allShown=false;
 
 let llamarBinance=async ()=>{
     let peticion = await fetch("https://api2.binance.com/api/v3/ticker/24hr", {
@@ -6,7 +7,7 @@ let llamarBinance=async ()=>{
     });
 
     if(peticion.status==200){
-        let datos= await peticion.json();
+        datos= await peticion.json();
         selectDatos(datos,symbols);
         console.log(select);
         genera_tabla();
@@ -14,13 +15,16 @@ let llamarBinance=async ()=>{
     }
     else{
         console.log(peticion.status);
+        
     }
 };
 
 let select=[];
+let selUSDT=[];
 
-let symbols=["BTCUSDT","ETHUSDT","ADAUSDT","LTCUSDT","SOLUSDT","XRPUSDT","BNBUSDT","AVAXUSDT","XLMUSDT"]
+let symbols=["BTCUSDT","ETHUSDT","ADAUSDT","LTCUSDT","SOLUSDT","XRPUSDT","BNBUSDT","AVAXUSDT","XLMUSDT","LUNAUSDT"]
 let selectDatos = (datos,symbols)=>{
+    select=[];
     
     for(let dato of datos)
     {
@@ -34,39 +38,97 @@ let selectDatos = (datos,symbols)=>{
 llamarBinance();
 
 
+let addCoin = ()=>{
+    symbol=document.getElementById("symbol").value + "USDT";
+    symbols.push(symbol);
+    llamarBinance();
+}
+
+let selectUSDT = ()=>{
+    selUSDT=[];
+    for(let moneda of datos){
+        if(moneda.symbol.includes("USDT"))
+        {
+            selUSDT.push(moneda);
+        }
+    }
+}
+
+
+let showAll=()=>{
+    selectUSDT();
+    select=selUSDT;
+    genera_tabla();
+}
+
+let alternar=()=>{
+    boton=document.getElementById("showAll");
+
+    if(allShown==false)
+    {
+        showAll();
+        allShown=true;
+        let texto=document.createTextNode("Mostrando todas");
+        boton.appendChild(texto);
+
+    }
+}
+
+let orderByGrowth = ()=>{
+
+
+}
+
 
 function genera_tabla() {
     // Obtener la referencia del elemento tbody
     tblBody=document.getElementsByTagName("tbody")[0];
+    tblBody.innerHTML=""; //Se vacía el tblBody para poder actualizarlo
     console.log(tblBody)
 
     // Crea las celdas
     for (let sel of select) {
-      // Crea las hileras de la tabla
-      let hilera = document.createElement("tr");
-      console.log(hilera);
+        // Crea las hileras de la tabla
+        let hilera = document.createElement("tr");
+        console.log(hilera);
 
-      
+        
+        let price=parseFloat(sel.lastPrice).toFixed(2).toString();
+        let info =[sel.symbol.replace('USDT',''),price,sel.priceChangePercent];
+        
+        for (let i of info) {
+            // Crea un elemento <td> y un nodo de texto, hace que el nodo de
+            // texto sea el contenido de <td>, ubica el elemento <td> al final
+            // de la hilera de la tabla
+            let celda = document.createElement("td");
+            celda.setAttribute("class","col-4 text-center")
 
-      let info =[sel.symbol,sel.lastPrice,sel.priceChangePercent];
-      
-      for (let i of info) {
-        // Crea un elemento <td> y un nodo de texto, hace que el nodo de
-        // texto sea el contenido de <td>, ubica el elemento <td> al final
-        // de la hilera de la tabla
-        let celda = document.createElement("td");
-        let textoCelda = document.createTextNode(i);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-        if(i==info[2]){
-            if(i.includes("-")){
-                celda.setAttribute("class","text-danger");
+            let textoCelda = document.createTextNode("");
+            
+            if(i==info[2]){
+                if(i.includes("-")){
+                    celda.setAttribute("class","text-danger col-4 text-center");
+                    textoCelda = document.createTextNode(i+" %");
+
+                }
+                else{
+                    celda.setAttribute("class","text-primary col-4 text-center");
+                    textoCelda = document.createTextNode("+"+i+" %");
+
+
+                }
+
+            }
+            else if(i==info[1])
+            {
+                textoCelda = document.createTextNode(i + " $");
+
             }
             else{
-                celda.setAttribute("class","text-primary");
-
+                textoCelda = document.createTextNode(i);
             }
-        }
+            celda.appendChild(textoCelda);
+            hilera.appendChild(celda);
         
       }
   
